dbdc88fb52011b8034cf516adba807cc5e9bccf4
kisung <abc1242@naver.com>
2022-04-06 23:30:21 +0900

    algo : 04/06

--- 코딩테스트준비/python/NDB_삽입정렬.py
+++ 코딩테스트준비/python/NDB_삽입정렬.py
@@ -2,9 +2,10 @@ array = [7,5,9,0,3,1,6,2,4,8]
 
 for i in range(1, len(array)):
     for j in range(i, 0, -1):
-        #자기 기준 왼쪽들 것과 비교하여 자기보다 큰 것을 찾고 그 왼쪽에 들어감
+        #한칸씩 왼쪽으로 이동하고
         if array[j] < array[j -1]:
             array[j], array[j-1] = array[j-1],array[j]
-        else:
+        else:   #자기보다 작은데이터를 만나면 멈춤
             break
+            
 print(array)
