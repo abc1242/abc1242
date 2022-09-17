0a406f88be818a5240ad55888c431deda04a24ff
kisung <abc1242@naver.com>
2022-09-17 15:51:53 +0900

    PROG_여행경로

---
+++ 코딩테스트준비/python/PROG_여행경로.py
@@ -0,0 +1,38 @@
+# https://gurumee92.tistory.com/165
+
+from collections import defaultdict
+
+def solution(tickets):
+    answer = []
+    
+    routes = defaultdict(list)
+    
+    
+    for start, end in tickets:
+
+        routes[start].append(end)
+    
+    for r in routes.keys():
+        routes[r].sort(reverse=True)
+    st = ['ICN']
+    path = []
+    
+    while st:
+        
+        top = st[-1]
+        
+        if top not in routes or len(routes[top]) == 0:
+            path.append(st.pop())
+        else:
+            st.append(routes[top][-1])
+            routes[top] = routes[top][:-1]
+    
+    
+    
+    answer = path[::-1]
+
+    
+    
+    
+    
+    return answer
