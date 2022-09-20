0c6858cf6814a23e61dcde1c3d292568f3d34a17
kisung <abc1242@naver.com>
2022-09-20 14:09:44 +0900

    BOJ_인구이동

--- 코딩테스트준비/python/BOJ_인구이동.py
+++ 코딩테스트준비/python/BOJ_인구이동.py
@@ -1,35 +1,80 @@
+import sys
+
+sys.setrecursionlimit(10**5)
+
+input = sys.stdin.readline
 
 N, L, R = map(int, input().split())
 
-# arr =[ list(map(int,input().split())) for _ in range(N)]
-
 arr = []
+
 for _ in range(N):
-    arr.append(list(map(int,input().split())))
 
-# print(arr)
+    arr.append(list(map(int, input().split())))
 
-visited = [ [False] * N for _ in range(N)]
+delta = [(1,0) , (-1,0) , (0,1) , (0,-1)]
 
-print(visited)
+v = [[False] * N for _ in range(N)]
 
-delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+tmp = []
+total = 0
 
-def bfs(i, j):
-    queue = [(i, j)]
+def move(r,c):
 
-    while queue:
-        r, c = queue.pop(0)
-        for dr, dc in delta:
-            nr = r + dr
-            nc = c + dc
-            print(nr, nc)
+    global total
 
 
-bfs(2, 3)
+    v[r][c] = True
 
-# while True:
-#     for i in range(N):
-#         for j in range(N):
-#             bfs(i,j)
-#
+    total += arr[r][c]
+    tmp.append([r,c])
+
+    for dr, dc in delta:
+
+        nr = r + dr
+        nc = c + dc
+
+        if 0 <= nr < N and 0<= nc < N and v[nr][nc] == False and L <= abs(arr[r][c] - arr[nr][nc]) <= R:
+
+            move(nr, nc)
+
+
+
+
+for cnt in range(N*N):
+
+    check = False
+    
+    for r in range(N):
+        
+        for c in range(N):
+            
+            if v[r][c] == False:
+
+                move(r,c)
+
+                Average = total // len(tmp)
+
+                for i, j in tmp:
+
+                    arr[i][j] = Average
+
+                if len(tmp) >=2:
+                    check = True
+
+                total = 0
+                tmp = []
+
+
+    v = [[False] * N for _ in range(N)]
+
+
+
+
+    if check == False:
+        print(cnt)
+
+        break
+    else:
+        check = False
+    
